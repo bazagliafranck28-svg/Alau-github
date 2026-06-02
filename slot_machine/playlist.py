cf4c26c21108330503319ad014cc2e45642f14a5
bazagliafranck28-svg <bazagliafranck28@gmail.com>
2026-06-01 22:02:20 -0300

    Aula 09

---
+++ slot_machine/playlist.py
@@ -0,0 +1,16 @@
+# import random
+# musicas =  ["Bohemian Rhapsody - Queen", "Imagine - John Lennon", "Stairway to Heaven - Led Zeppelin", "Hey Jude - The Beatles", "Smells Like Teen Spirit - Nirvana"]
+# laços = 0
+# print("=== Playlist do Senai ===\n")
+# while laços < 5:
+#     input ("\npressione ENTER para tocar a próxima música...")
+#     laços += 1
+
+#     resultado = random.choice(musicas)
+#     print(f"Tocando: {resultado}")
+
+playlist = ["camila", "Veneto no litoral","Há Tempos","Sobre o tempo"]
+
+for musica in playlist:
+    print(musica)
+    break
---
+++ slot_machine/registro.py
@@ -0,0 +1,20 @@
+usuarios = [
+    ["Alice", "alice@gmail.com", 30],
+    ["bob", "bob@gmail.com", 24],
+    ["carlos", "carlos@gmail.com", 35],
+    ["Diana", "diana@yahoo.com", 28]
+   
+]
+
+# print(usuarios)
+
+primeiro_usuario = usuarios[0]
+print(f"primeiro usuario: {primeiro_usuario}")
+
+segundo_usuario = usuarios[1][0]
+print(f"segundo usuario: {segundo_usuario}")
+
+quarto_usuario_email = usuarios[3][1]
+print(f"quarto usuario email: {quarto_usuario_email}")
+
+ 
